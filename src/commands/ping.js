7dd64c6e3630bbd091323df9cb73474c64332bc9
Max Hurst <max.hurst@hotmail.com>
2020-11-01 18:12:24 +0000

    commands: ping command

---
+++ src/commands/ping.js
@@ -0,0 +1,20 @@
+const Command = require('../structures/bases/commandBase');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            name: 'ping',
+            description: 'Pong!',
+            category: 'Util',
+        });
+    }
+
+    async execute(message) {
+        const msg = await message.channel.send('Pinging...');
+
+        const messagePing = msg.createdTimestamp - message.createdTimestamp;
+
+        msg.edit(`🏓 Pong! \`${messagePing}ms\` \nHeart beat: \`${this.client.ws.ping}ms\``);
+    }
+}; 
