c830a60026ba3db8bfd879b8b15f32068ec07e30
Max Hurst <max.hurst@hotmail.com>
2020-11-01 20:15:15 +0000

    command: ping command refactor

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -13,9 +13,7 @@ module.exports = class extends Command {
 
     async execute(message) {
         const msg = await message.channel.send('Pinging...');
-
         const messagePing = msg.createdTimestamp - message.createdTimestamp;
-
         msg.edit(`🏓 Pong! \`${messagePing}ms\` \nHeart beat: \`${this.client.ws.ping}ms\``);
     }
 }; 
