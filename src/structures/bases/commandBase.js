c8763dadd5803f30a9d74cf5d9c0a8a6dc2e31d8
Max Hurst <max.hurst@hotmail.com>
2020-11-01 18:14:50 +0000

    bases: added cooldowns + guildOnly to command base

--- src/structures/bases/commandBase.js
+++ src/structures/bases/commandBase.js
@@ -5,12 +5,14 @@ module.exports = class Command {
         this.name = options.name || name;
         this.description = options.description || null;
         this.aliases = options.aliases || [];
-        this.usage = options.usage || null;
         this.category = options.category || null;
         this.permission = options.permission || null;
+        this.cooldown = options.cooldown || null;
+        this.usage = options.usage || null;
         this.disabled = options.disabled || false;
         this.nsfw = options.nsfw || false;
         this.ownerOnly = options.ownerOnly || false;
+        this.guildOnly = options.guildOnly || false;
     }
 
 }; 
