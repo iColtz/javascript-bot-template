567ffca1297591a37b666bf6cc507c917ce4dfe3
iColtz <max.hurst@hotmail.com>
2020-12-05 12:56:19 +0000

    classes(Command): cooldown

--- src/struct/Command.js
+++ src/struct/Command.js
@@ -5,6 +5,7 @@ class Command {
     this.guildOnly = Boolean(options.guildOnly) || true;
     this.ownerOnly = Boolean(options.ownerOnly) || false;
     this.requiredArgs = Number(options.requiredArgs) || 0;
+    this.cooldown = Number(options.cooldown) || 0;
     this.userPermissions = options.userPermissions || [];
     this.clientPermissions = options.clientPermissions || [];
   }
