53837ec7a59039fc63ab3666a6fe21e7e0060e89
iColtz <max.hurst@hotmail.com>
2020-12-05 13:20:45 +0000

    classes(Command): usage

--- src/struct/Command.js
+++ src/struct/Command.js
@@ -3,6 +3,7 @@ class Command {
     this.id = options.id || '';
     this.aliases = options.aliases || [];
     this.description = options.description || null;
+    this.usage = options.usage || null;
     this.guildOnly = Boolean(options.guildOnly) || true;
     this.ownerOnly = Boolean(options.ownerOnly) || false;
     this.requiredArgs = Number(options.requiredArgs) || 0;
