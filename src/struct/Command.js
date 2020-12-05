d0e4d07008e2755922c9fbc3a45c824440d226d4
iColtz <max.hurst@hotmail.com>
2020-12-05 12:24:29 +0000

    classes(command): guildOnly

--- src/struct/Command.js
+++ src/struct/Command.js
@@ -2,6 +2,7 @@ class Command {
   constructor(options) {
     this.id = options.id || '';
     this.aliases = options.aliases || [];
+    this.guildOnly = Boolean(options.guildOnly) || true;
   }
 }
 
