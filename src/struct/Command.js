eff7ff2ad8994dff60bc9c2e6be3fbfa11913836
iColtz <max.hurst@hotmail.com>
2020-12-05 13:21:12 +0000

    classes(Command): category

--- src/struct/Command.js
+++ src/struct/Command.js
@@ -4,6 +4,7 @@ class Command {
     this.aliases = options.aliases || [];
     this.description = options.description || null;
     this.usage = options.usage || null;
+    this.category = options.category || null;
     this.guildOnly = Boolean(options.guildOnly) || true;
     this.ownerOnly = Boolean(options.ownerOnly) || false;
     this.requiredArgs = Number(options.requiredArgs) || 0;
