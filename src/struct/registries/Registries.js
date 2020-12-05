32e05c881bf783562cc3e6b0a7d6cb12c10fe3ec
iColtz <max.hurst@hotmail.com>
2020-12-05 11:27:33 +0000

    registries(registerEvents)

---
+++ src/struct/registries/Registries.js
@@ -0,0 +1,17 @@
+const { sync } = require('glob');
+const { resolve } = require('path');
+const Event = require('../Event.js');
+
+function registerEvents(client) {
+  const eventFiles = sync(resolve('./src/events/**/*.js'));
+  eventFiles.forEach((filepath) => {
+    const File = require(filepath);
+    if (!(File.prototype instanceof Event)) return;
+    const event = new File();
+    client[event.type ? 'once' : 'on'](event.id, (...args) => event.exec(...args));
+  });
+}
+
+module.exports = {
+  registerEvents
+};
