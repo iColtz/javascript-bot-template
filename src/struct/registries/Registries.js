9ca56237b801a00bea767e7a9b0f947e6ad70b46
iColtz <max.hurst@hotmail.com>
2020-12-05 11:32:18 +0000

    Registries(Events): setting the event in the events collection

--- src/struct/registries/Registries.js
+++ src/struct/registries/Registries.js
@@ -8,6 +8,7 @@ function registerEvents(client) {
     const File = require(filepath);
     if (!(File.prototype instanceof Event)) return;
     const event = new File();
+    client.events.set(event.id, event);
     client[event.type ? 'once' : 'on'](event.id, (...args) => event.exec(...args));
   });
 }
