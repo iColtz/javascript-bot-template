6fce9ccb28a6f1412e8e6a30a78859227892adad
iColtz <max.hurst@hotmail.com>
2020-12-05 12:28:31 +0000

    classes(Command): added ownerOnly

--- src/struct/Command.js
+++ src/struct/Command.js
@@ -3,6 +3,7 @@ class Command {
     this.id = options.id || '';
     this.aliases = options.aliases || [];
     this.guildOnly = Boolean(options.guildOnly) || true;
+    this.ownerOnly = Boolean(options.ownerOnly) || false;
   }
 }
 
