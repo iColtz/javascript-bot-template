5c2c72ffcfd017905b995ee6aadb6421a336dd58
iColtz <max.hurst@hotmail.com>
2020-12-05 11:39:45 +0000

    registries(Command)

--- src/struct/registries/Registries.js
+++ src/struct/registries/Registries.js
@@ -1,6 +1,7 @@
 const { sync } = require('glob');
 const { resolve } = require('path');
 const Event = require('../Event.js');
+const Command = require('../Command.js');
 
 function registerEvents(client) {
   const eventFiles = sync(resolve('./src/events/**/*.js'));
@@ -13,6 +14,17 @@ function registerEvents(client) {
   });
 }
 
+function registerCommands(client) {
+  const commandFiles = sync(resolve('./src/commands/**/*.js'));
+  commandFiles.forEach((filepath) => {
+    const File = require(filepath);
+    if (!(File.prototype instanceof Command)) return;
+    const command = new File();
+    client.commands.set(command.id, command);
+  });
+}
+
 module.exports = {
-  registerEvents
+  registerEvents,
+  registerCommands,
 };
